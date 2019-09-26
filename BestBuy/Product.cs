4c6b279bdfa5cc7c9e49bb539fe5f5f620123ca6
Troy Maxwell <zephyrmaxwell@hotmail.com>
2019-09-26 11:01:27 -0500

    made interface, class,implemented both

---
+++ BestBuy/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuy
+{
+    class Product : IProduct
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }        
+        public Product(string name, double price)
+        {
+            //Constructor get's and set's Product Name and Product Price
+            Name = name;
+            Price = price;
+        }
+        public double PriceWithTax()
+        {
+            Price *= 1.1;
+            Price = Math.Round(Price, 2);
+            return Price;
+        }
+}
+}
---
+++ BestBuy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BestBuy
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product newProduct = new Product("IPhone", 349.99);
+            /*newProduct.Name = "Iphone";
+            newProduct.Price = 349;
+            newProduct.PriceWithTax();*/
+
+            Console.WriteLine($"{newProduct.Name}, Price: ${newProduct.Price}, Price with tax:{newProduct.PriceWithTax()}");
+        }
+    }
+}
